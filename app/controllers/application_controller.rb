54f5e1df0fede4db23914508a7f6742d40287d66
Joe King <j.king92@gmail.com>
2023-02-14 15:10:26 -0700

    fix: issue with admin dashboard link not working on admin user show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user # this gives access to the method in the views!! mind blown
   def current_user
-    @user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
 
     # ||= is memoization, after the first time you call the method it will check if it has been defined within
     # the request. It caches the value for the duration of the request so we don't have to find the user 10x
