e820cd3b94620d9de3126684fa2df8181ef567aa
Joe King <j.king92@gmail.com>
2023-02-14 17:38:18 -0700

    refactor: Reorganize routes for readability

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get '/dashboard', to: 'users#show'
+  get '/login', to: 'users#login_form'
+  post '/login', to: 'users#login_user'
+  get '/logout', to: 'users#logout_user'
+  get '/register', to: 'users#new'
 
   resources :users, only: :create
 
@@ -11,15 +15,10 @@ Rails.application.routes.draw do
     resources :viewing_party, only: %i[new create show]
   end
 
-  get '/admin/dashboard', to: 'admin/users#index'
   namespace :admin do
     resources :users, only: :show
+    get '/dashboard', to: 'users#index'
   end
 
   resources :discover, only: :index
-  get '/login', to: 'users#login_form'
-  post '/login', to: 'users#login_user'
-  get '/logout', to: 'users#logout_user'
-
-  get '/register', to: 'users#new'
 end
