3228b0eb02380f73b647ad5f7db3b2f5d2f1b126
Joe King <j.king92@gmail.com>
2023-02-14 17:22:03 -0700

    remove comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  helper_method :current_user # this gives access to the method in the views!! mind blown
+  helper_method :current_user
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-
-    # ||= is memoization, after the first time you call the method it will check if it has been defined within
-    # the request. It caches the value for the duration of the request so we don't have to find the user 10x
-    # to load the page a single time.
   end
 
   def validate_user
