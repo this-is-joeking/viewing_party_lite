2f428e758a30d2b40dda72fb84452f98814e4f4d
Joe King <j.king92@gmail.com>
2023-04-05 18:34:28 -0700

    chore: remove home text and set image to link to home

--- db/schema.rb
+++ db/schema.rb
@@ -1,49 +1,48 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_230_214_203_429) do
+ActiveRecord::Schema.define(version: 2023_02_14_203429) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'password_digest'
-    t.integer 'role', default: 0
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.integer "role", default: 0
   end
 
-  create_table 'viewing_parties', force: :cascade do |t|
-    t.integer 'movie_id'
-    t.date 'date'
-    t.time 'start_time'
-    t.integer 'party_duration'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "viewing_parties", force: :cascade do |t|
+    t.integer "movie_id"
+    t.date "date"
+    t.time "start_time"
+    t.integer "party_duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'viewing_party_users', force: :cascade do |t|
-    t.bigint 'user_id'
-    t.bigint 'viewing_party_id'
-    t.boolean 'hosting'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_viewing_party_users_on_user_id'
-    t.index ['viewing_party_id'], name: 'index_viewing_party_users_on_viewing_party_id'
+  create_table "viewing_party_users", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "viewing_party_id"
+    t.boolean "hosting"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_viewing_party_users_on_user_id"
+    t.index ["viewing_party_id"], name: "index_viewing_party_users_on_viewing_party_id"
   end
 
-  add_foreign_key 'viewing_party_users', 'users'
-  add_foreign_key 'viewing_party_users', 'viewing_parties'
+  add_foreign_key "viewing_party_users", "users"
+  add_foreign_key "viewing_party_users", "viewing_parties"
 end
